760c0eea9b6b4905ded67a6054cf63ca042899bb
iton0 <iton224@protonmail.com>
2025-10-29 22:11:18 -0400

    better handle possible cd error

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd shared
+cd shared || exit 1
 go mod tidy
-cd ../key-gen-service
+cd ../key-gen-service || exit 1
 go mod tidy
-cd ../url-shortener-service
+cd ../url-shortener-service || exit 1
 go mod tidy
-cd ../url-redirect-service
+cd ../url-redirect-service || exit 1
 go mod tidy
-cd ../api-gateway-service
+cd ../api-gateway-service || exit 1
 go mod tidy
-cd ..
+cd .. || exit 1
